f27bff6a45b0c3ec7e66e2731e855227b4749e95
Szabolcs Tóth <tothszabi0224@gmail.com>
2026-03-20 16:01:22 +0100

    pgAdmin letöltése, Docker Dekstop már rajta volt a gépen, docker-compose.yml fájl létrehozása, konténer létrehozása és adatb regisztrálása pgAdminban, majd tábla létrehozása (Fixed #2)

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-import express from "express";
+//endpointok kezelése,
+
+import express from 'express';
+import pool from './config/db.js';
 
 const app = express();
+
+pool.query("SELECT NOW()", (err, result) => {
+    if(err) {
+        console.log("hiba: ", err);
+    } else {
+        console.log("ok", result.rows);
+    }
+});
 
 app.get('/', (req, res) => {
     res.send("szihelo");
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//backend futtatás a 8080-as potron
+
 import app from "./app.js";
 
 const port = 8080;
